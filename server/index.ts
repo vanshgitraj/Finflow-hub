5093cd033e05236863863c6c8fc791c50907a93e
vanshgitraj <vanshraj01089@gmail.com>
2025-06-16 19:06:32 +0530

    index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,3 @@
-
-import loanApplicationRoute from './routes/loanApplicationRoute';
-app.use(loanApplicationRoute);
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
