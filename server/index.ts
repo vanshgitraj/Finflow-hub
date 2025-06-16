c83d5d799284048572c9853b3017c64e5da1357a
vanshgitraj <vanshraj01089@gmail.com>
2025-06-16 18:59:23 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import loanApplicationRoute from './routes/loanApplicationRoute';
+app.use(loanApplicationRoute);
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
